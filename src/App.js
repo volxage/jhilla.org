40be85d1a87c0e023e3668be210357acb4acff83
volxage <hillia45@msu.edu>
2024-06-16 21:42:41 -0400

    Slightly modified react default example :)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          jhilla.org is about to be killin.......
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
