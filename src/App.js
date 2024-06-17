4a49d853f01c0d2731c559052e731f617225d287
hillia45 <hillia45@msu.edu>
2024-06-17 15:36:12 -0400

    Added links, expirimenting with Tailwind

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link href="./output.css" rel="stylesheet"/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jhilla.org is about to be killin.......
+        <p class="text-3xl font-bold">
+          jhilla.org, for computer science and bass
         </p>
+        <div className="bg-white shadow p-4">
+        <p><a href='https://www.github.com/volxage'>Github</a></p>
+        <p><a href='https://www.instagram.com/jhilla.bass'>Instagram</a></p>
+        </div>
       </header>
     </div>
   );
