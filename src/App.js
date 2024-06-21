b3d3652574d616bf869badd6a06ec5dc9ce8085a
volxage <hillia45@msu.edu>
2024-06-20 20:25:48 -0400

    Added bass clef, styled buttons to Github & Insta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import bass from './bass.svg';
 import './App.css';
 
 function App() {
@@ -6,13 +7,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <link href="./output.css" rel="stylesheet"/>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='flex flex-row m-16'>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div>
+            <img src={bass} className="App-logo" alt="logo" />
+          </div>
+        </div>
         <p class="text-3xl font-bold">
           jhilla.org, for computer science and bass
         </p>
-        <div className="bg-white shadow p-4">
-        <p><a href='https://www.github.com/volxage'>Github</a></p>
-        <p><a href='https://www.instagram.com/jhilla.bass'>Instagram</a></p>
+        <p class="text-2xl">
+          hillia45@msu.edu
+        </p>
+        <div className=''>
+          <a href='https://www.github.com/volxage'>
+            <div className="bg-cyan-800 shadow rounded-md py-2 px-4 m-8">
+              <p>Github</p>
+            </div>
+          </a>
+          <a href='https://www.instagram.com/jhilla.bass'>
+            <div className="bg-cyan-800 shadow rounded-md py-2 px-4 m-8">
+              <p><a href='https://www.instagram.com/jhilla.bass'>Instagram</a></p>
+            </div>
+          </a>
         </div>
       </header>
     </div>
---
+++ src/App.sync-conflict-20240617-165352-G5RRMFX.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {Text, SubText} from './Style.tsx';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          jhilla.org is about to be killin.......
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
