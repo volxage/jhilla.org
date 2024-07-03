76e0da67d94d16e02e5bcdf65e496fee7a5152af
hillia45 <hillia45@msu.edu>
2024-07-03 10:17:14 -0400

    Removed instagram link, added jhilla@jhilla.org

--- src/App.js
+++ src/App.js
@@ -18,18 +18,13 @@ function App() {
         <p class="text-3xl font-bold">
           jhilla.org, for computer science and bass
         </p>
-        <p class="text-2xl">
-          hillia45@msu.edu
+        <p class="text-xl">
+          jhilla@jhilla.org
         </p>
         <div className=''>
           <a href='https://www.github.com/volxage'>
             <div className="bg-cyan-800 shadow rounded-md py-2 px-4 m-8">
               <p>Github</p>
-            </div>
-          </a>
-          <a href='https://www.instagram.com/jhilla.bass'>
-            <div className="bg-cyan-800 shadow rounded-md py-2 px-4 m-8">
-              <p><a href='https://www.instagram.com/jhilla.bass'>Instagram</a></p>
             </div>
           </a>
         </div>
