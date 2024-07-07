c1b12a2af6f522fe3e70b3077ecdfac8c026439b
volxage <hillia45@msu.edu>
2024-07-06 23:29:37 -0400

    Removed junk, added JF prototype

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import logo from './logo.svg';
 import bass from './bass.svg';
 import './App.css';
+import jhillaFloating from './floatingjhilla.png';
+import museum from './museum.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <link href="./output.css" rel="stylesheet"/>
-        <div className='flex flex-row m-16'>
-          <div>
-            <img src={logo} className="App-logo" alt="logo" />
+    <body class="App bg-gradient-to-b from-brown-50">
+      <link href="./output.css" rel="stylesheet"/>
+      <div class='grid place-items-center'>
+        <div class="relative h-[67vw] w-[80vw] flex">
+          <img class="absolute inset-0 object-contain" src={museum} />
+          <div class="absolute inset-0 right-[8vw]">
+            <p class="text-[17vw] drop-shadow-2xl font-extrabold font-sans text-brown-50 italic">
+              JHILLA
+            </p>
+            <p class="text-[22vw] font-extrabold m-[-13vw] text-brown-50 italic">
+              BASS.
+            </p>
           </div>
-          <div>
-            <img src={bass} className="App-logo" alt="logo" />
-          </div>
+          <img class="absolute inset-0 object-contain" src={jhillaFloating} />
         </div>
-        <p class="text-3xl font-bold">
-          jhilla.org, for computer science and bass
-        </p>
-        <p class="text-xl">
-          jhilla@jhilla.org
-        </p>
-        <div className=''>
-          <a href='https://www.github.com/volxage'>
-            <div className="bg-cyan-800 shadow rounded-md py-2 px-4 m-8">
-              <p>Github</p>
-            </div>
-          </a>
-        </div>
-      </header>
+      <p class="text-4xl font-light m-5">
+        jhilla@jhilla.org
+      </p>
+    <div class="text-xl w-3/5">
+      Jonathan Hilliard is a bassist/computer scientist from the Philadelphia area. He is studying Jazz and Computer Science with the MSU faculty. He is currently developing an App named TuneTracker, whose progress can be monitored
+      <span class="inline">
+        <a class="inline text-brown-50" href='https://www.github.com/volxage/tunetracker'> here.</a>
+      </span>
     </div>
+      </div>
+    </body>
   );
 }
 
